ecf0aa1c56182f33d95d00579b05f493c0305150
maphy9 <maphy9@gmail.com>
2025-05-02 15:47:28 +0200

    Add ReturnButton component

---
+++ frontend/src/components/SharedComponents/ReturnButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="return-button">
+    <span class="return-button-text">Go back to main menu</span>
+
+    <img class="return-button-icon" src="@/images/home.png">
+  </div>
+</template>
+
+<style scoped>
+.return-button {
+  cursor: pointer;
+  position: fixed;
+  bottom: 12px;
+  border: 1px solid white;
+  width: 426px;
+  height: 70px;
+  background-color: #8B231D;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.3s ease;
+}
+
+.return-button:hover {
+  background-color: #7b1c17;
+}
+
+.return-button:active {
+  transform: scale(1.05);
+  background-color: #9a2c26;
+}
+
+.return-button-text {
+  text-transform: upper;
+  color: white;
+  font-size: 28px;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  font-weight: bold;
+  -webkit-text-stroke-width: 1px;
+  -webkit-text-stroke-color: black;
+}
+
+.return-button-icon {
+  width: 70px;
+  height: 70px;
+}
+
+@media only screen and (max-width: 550px) {
+  .return-button {
+    display: none;
+  }
+}
+</style>

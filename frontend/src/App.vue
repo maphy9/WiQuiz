11b8809b5651ee85671c493721a6050bb0db5822
maphy9 <maphy9@gmail.com>
2025-05-02 15:31:54 +0200

    Add global css rules

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,20 @@
 <template>
   Empty project
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap');
+
+* {
+  padding: 0;
+  margin: 0;
+  border: 0;
+  font-family: "Titillium Web", sans-serif;
+  font-style: normal;
+}
+
+html, body, #app {
+  width: 100vw;
+  height: 100vh;
+}
+</style>

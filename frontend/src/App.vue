246e275a243edb37812a91679baccda28dec6198
Mikita Dzeviatau <81618202+mikita672@users.noreply.github.com>
2025-05-06 11:41:17 +0200

    Add one-way animation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <router-view />
+  <div class="view-wrapper">
+    <router-view v-slot="{ Component }">
+      <transition name="swipe-down">
+        <component :is="Component" :key="$route.fullPath" class="view" />
+      </transition>
+    </router-view>
+  </div>
 </template>
 
 <style>
@@ -18,5 +24,47 @@ body,
 #app {
   width: 100vw;
   height: 100vh;
+  overflow: hidden;
+}
+
+.view {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.swipe-down-enter-active,
+.swipe-down-leave-active {
+  transition: transform 0.5s ease;
+}
+.swipe-down-enter-from,
+.swipe-down-leave-from,
+.swipe-down-enter-to,
+.swipe-down-leave-to {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+.swipe-down-enter-from {
+  transform: translateY(-100%);
+  z-index: 2;
+}
+.swipe-down-enter-to {
+  transform: translateY(0);
+  z-index: 2;
+}
+
+.swipe-down-leave-from {
+  transform: translateY(0);
+  z-index: 1;
+}
+.swipe-down-leave-to {
+  transform: translateY(100%);
+  z-index: 1;
 }
 </style>

d42e4e2ce46c60c24dd55a4626b826e9af410b0d
Malikethes <malynovskytdmitriyy@gmail.com>
2025-05-24 04:00:20 +0200

    Add background music and sound effects for all components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,8 +15,29 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+import { ref, onMounted } from 'vue'
 
 const route = useRoute()
+
+const mainTheme = ref<HTMLAudioElement | null>(null)
+const mainThemeVolume = ref(0.1)
+
+onMounted(() => {
+  if(!(window as any).musicIsPlaying) {
+    mainTheme.value = new Audio('/src/sounds/mainTheme.mp3')
+    mainTheme.value.volume = mainThemeVolume.value
+    mainTheme.value.loop = true
+
+    const playMainTheme = () => {
+      if (mainTheme.value){
+        mainTheme.value.play().catch(() => {})
+        ;(window as any).musicIsPlaying = true
+      }
+      window.removeEventListener('click', playMainTheme)
+    }
+    window.addEventListener('click', playMainTheme)
+  }
+})
 
 const currentTransition = computed(() => {
   return route.name === 'main'
